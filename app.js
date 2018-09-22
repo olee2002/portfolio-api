882dbdd659a16a7f726d6db2a4679eba3807da2a
babylee2002 <babylee2002@gmail.com>
2018-09-22 16:30:01 -0400

    test

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', cors({ origin: 'https://olee-portfolio.herokuapp.com/' }), usersRouter);
+app.use('/api', cors({ origin: 'https://olee-portfolio-api.herokuapp.com/' }), usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
