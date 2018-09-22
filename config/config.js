5946c936acd4a50ae3c680a03025cf399f964ded
babylee2002 <babylee2002@gmail.com>
2018-09-22 02:42:23 -0400

    config changed from json to js

---
+++ config/config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  development: {
+    username: "olee",
+    database: "portfolio",
+    dialect: "postgres",
+    host: "127.0.0.1",
+    logging: false
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL'
+  }
+};
