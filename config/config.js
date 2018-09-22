88512abc3f51940a1eac12da4d521b0061926553
babylee2002 <babylee2002@gmail.com>
2018-09-22 02:48:54 -0400

    config corrected

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: 'mysql',
+    dialect: "postgres",
     use_env_variable: 'DATABASE_URL'
   }
 };
