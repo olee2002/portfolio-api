8f889a591320c4de8415244d97fc92ba907297d9
babylee2002 <babylee2002@gmail.com>
2018-09-22 00:56:03 -0400

    express sequelize set up

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    user_id: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {});
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+
+const Model = require('../models')
+console.log(Model.user)
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/test', async (req, res, next) => {
+  const users = await Model.user.findAll({});
+  res.json(users);
+});
+router.post('/test', async (req, res, next) => {
+  console.log('req.body', req.body)
+  // const users = await Model.user.create({});
+  // res.json(users);
+});
+
+module.exports = router;
