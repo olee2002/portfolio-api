393ddb51372c317dd27bdc027f0524e1c1076a02
babylee2002 <babylee2002@gmail.com>
2018-09-24 23:51:46 -0400

    cors test

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', cors({ origin: '*' }), apiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
